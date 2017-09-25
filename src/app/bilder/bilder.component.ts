34e61590d8d6d2f9c06fe4af0f3f512e66a26819
oskar <oskar@mangosia.com>
2017-09-25 06:16:16 +0200

    her er de nye sidene

---
+++ src/app/bilder/bilder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bilder',
+  templateUrl: './bilder.component.html',
+  styleUrls: ['./bilder.component.css']
+})
+export class BilderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
