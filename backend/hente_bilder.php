8f6e3622b5808061a7b3f695f31d4767cd6b97a8
oskar <oskar@mangosia.com>
2017-10-03 15:48:00 +0200

    bere tok å fiksa utsenet på koden

--- backend/hente_bilder.php
+++ backend/hente_bilder.php
@@ -5,22 +5,22 @@
     $dbname = "bilder";
 
     // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
 
-$sql = "SELECT id, src, title FROM bilder";
-$result = $conn->query($sql);
+    $sql = "SELECT id, src, title FROM bilder";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - fill: " . $row["src"]. " - title: " . $row["title"]. "<br>";
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - fill: " . $row["src"]. " - title: " . $row["title"]. "<br>";
+        }
+    } else {
+        echo "0 results";
     }
-} else {
-    echo "0 results";
-}
-$conn->close();
+    $conn->close();
 ?>
