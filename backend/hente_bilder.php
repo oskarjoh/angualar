0dcd4bd44c77e78094c65d78aacbed6d91db838a
oskar <oskar@mangosia.com>
2017-10-03 15:37:22 +0200

    nå bere printer den alt som er i databasen

--- backend/hente_bilder.php
+++ backend/hente_bilder.php
@@ -1,6 +1,26 @@
 <?php
-    $host = "192.168.0.106";
-    $user = "bild";
+    $servername = "192.168.0.106";
+    $username = "bild";
     $password = "Bilder1!";
-    $db = "bilder";
+    $dbname = "bilder";
+
+    // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT id, src, title FROM bilder";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - fill: " . $row["src"]. " - title: " . $row["title"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
