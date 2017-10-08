3495db23c84b806de7bd6f4138e58c2db9e2fbef
oskar <oskar@mangosia.com>
2017-10-08 17:49:09 +0200

    endra en ting i ifen

--- src/app/bilder/bilder.component.ts
+++ src/app/bilder/bilder.component.ts
@@ -12,17 +12,18 @@ export class BilderComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  test = 2;
   b = 'bilder';
   akra(){
     this.h3tekst="Akra";
   }
   // skal velge vilken bilder som blir vist
   bid(){
-    if (5 < 3) {
+    if (this.test < 3) {
       this.h3tekst="if funker";
     }
   }
+
   bsrc = [
     {src:"https://oskar.mangosia.com/web/data/bilder/drone/DJI_0016.JPG"},
     {src: "https://oskar.mangosia.com/web/data/bilder/drone/DJI_0017.JPG"},
