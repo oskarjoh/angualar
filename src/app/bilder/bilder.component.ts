432618032fe27b2967c7cc32c671c272a5fff7d8
oskar <oskar@mangosia.com>
2017-10-08 05:52:33 +0200

    lagt til en knap som starter et skript

--- src/app/bilder/bilder.component.ts
+++ src/app/bilder/bilder.component.ts
@@ -17,6 +17,12 @@ export class BilderComponent implements OnInit {
   akra(){
     this.h3tekst="Akra";
   }
+  // skal velge vilken bilder som blir vist
+  bid(){
+    if (1 < 3) {
+      this.h3tekst="if funker";
+    }
+  }
   bsrc = [
     {src:"https://oskar.mangosia.com/web/data/bilder/drone/DJI_0016.JPG"},
     {src: "https://oskar.mangosia.com/web/data/bilder/drone/DJI_0017.JPG"},
